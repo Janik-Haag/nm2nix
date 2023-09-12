aebaaef004a4d29918f0ebe8d746704b866c3826
Janik H <janik@aq0.de>
2023-09-12 17:38:40 +0200

    fix aarch64-flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
   {
-    packages.x86_64-linux.default = pkgs.writers.writePython3Bin "nm2nix" { } (builtins.readFile ./nm2nix.py);
-    packages.aarch64-linux.default = pkgs.writers.writePython3Bin "nm2nix" { } (builtins.readFile ./nm2nix.py);
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.writers.writePython3Bin "nm2nix" { } (builtins.readFile ./nm2nix.py);
+    packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.writers.writePython3Bin "nm2nix" { } (builtins.readFile ./nm2nix.py);
   };
 }
