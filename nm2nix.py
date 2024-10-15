4ffc36b3292bed1c8348aa09b2a9ecd0d9f4dfb8
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-15 23:40:51 +0100

    Refactor JSON to Nix handling
    This commit a) fixes issue #8, and b) wraps the `nix-instantiate` call
    in a Python `with` block, which is easier to read than using `procfs`.
    It also means the `nix-instantiate` call is portable.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- nm2nix.py
+++ nm2nix.py
@@ -1,4 +1,4 @@
-from os import listdir, getpid
+from os import listdir
 from subprocess import check_output
 from os.path import isfile, join
 import configparser
@@ -22,9 +22,7 @@ for i in nmfiles:
         for key in config[section]:
             jsonConfigs[connection_name][section][key] = config[section][key]
 
-tf = tempfile.TemporaryFile("w+")
-jsonConfigs = json.dump(jsonConfigs, tf)
-tf.flush()
-
-
-print(check_output(["nix-instantiate", "--expr", "--eval",  f"builtins.fromJSON (builtins.readFile \"/proc/{getpid()}/fd/{tf.fileno()}\")"], text=True))  # noqa: E501
+with tempfile.NamedTemporaryFile(mode="w") as tf:
+    tf.write(json.dumps(jsonConfigs))
+    tf.flush()
+    print(check_output(["nix-instantiate", "--expr", "--eval",  f"builtins.fromJSON (builtins.readFile \"{tf.name}\")"], text=True))  # noqa: E501
