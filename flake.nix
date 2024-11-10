bbfd56bb7522eabb58c14db2c9c682f7f98b6b2b
Victor Engmark <victor@engmark.name>
2024-11-10 16:23:51 +1300

    feat: Add dev shell

--- flake.nix
+++ flake.nix
@@ -25,5 +25,21 @@
           default = pkgs.writers.writePython3Bin "nm2nix" { } (builtins.readFile ./nm2nix.py);
         }
       );
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.black
+              pkgs.isort
+              pkgs.nixfmt-rfc-style
+              pkgs.ruff
+            ];
+            shellHook = ''
+              ln --force --no-target-directory --symbolic "${pkgs.python3}/bin/python" python
+            '';
+          };
+        }
+      );
     };
 }
