07095452c2deae9598fefe015433b200befb2b66
Victor Engmark <victor@engmark.name>
2024-11-10 16:04:28 +1300

    refactor: Reformat using nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }:
-  {
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.writers.writePython3Bin "nm2nix" { } (builtins.readFile ./nm2nix.py);
-    packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.writers.writePython3Bin "nm2nix" { } (builtins.readFile ./nm2nix.py);
-  };
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages.x86_64-linux.default =
+        nixpkgs.legacyPackages.x86_64-linux.writers.writePython3Bin "nm2nix" { }
+          (builtins.readFile ./nm2nix.py);
+      packages.aarch64-linux.default =
+        nixpkgs.legacyPackages.aarch64-linux.writers.writePython3Bin "nm2nix" { }
+          (builtins.readFile ./nm2nix.py);
+    };
 }
