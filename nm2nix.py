1b0df79b9c4148e5ccb2cd7b9eab010595c78637
Victor Engmark <victor@engmark.name>
2025-04-10 18:05:18 +0200

    feat: Rework script
    - Use logger module for debugging purposes.
    - Read files from standard `/etc/NetworkManager/system-connections`
      directory rather than current directory, for ease of use.
    - Use temporary file context manager to delete after use.

--- nm2nix.py
+++ nm2nix.py
@@ -1,37 +1,47 @@
-import configparser
-import json
-import tempfile
-from os import listdir
-from os.path import isfile, join
+from configparser import ConfigParser
+from glob import glob
+from json import dump
+from logging import DEBUG, basicConfig, getLogger
+from os.path import join
 from subprocess import check_output
+from tempfile import NamedTemporaryFile
 
-path = "./"
+basicConfig(level=DEBUG)
+LOGGER = getLogger(__name__)
 
-files = [f for f in listdir(path) if isfile(join(path, f))]
-nmfiles = [f for f in files if f.endswith(".nmconnection")]
+CONNECTIONS_DIRECTORIES = [
+    "/etc/NetworkManager/system-connections",
+    "/run/NetworkManager/system-connections",
+]
+EXTENSION = "nmconnection"
+GLOBS = [join(directory, f"*.{EXTENSION}") for directory in CONNECTIONS_DIRECTORIES]
 
-jsonConfigs = {}
+configs = {}
 
-for i in nmfiles:
-    config = configparser.ConfigParser(delimiters=("=",))
-    config.read(i)
-    connection_name = i.removesuffix(".nmconnection")
-    jsonConfigs[connection_name] = {}
-    for section in config.sections():
-        jsonConfigs[connection_name][section] = {}
-        for key in config[section]:
-            jsonConfigs[connection_name][section][key] = config[section][key]
+for glob_ in GLOBS:
+    LOGGER.debug(f"Looping through {glob_}")
+    for file_path in glob(glob_):
+        LOGGER.info(f"Handling {file_path}")
+        config = ConfigParser(delimiters=("=",))
+        config.read(file_path)
+        connection_name = file_path.removesuffix(f".{EXTENSION}")
+        configs[connection_name] = {}
+        for section in config.sections():
+            configs[connection_name][section] = {}
+            for key in config[section]:
+                configs[connection_name][section][key] = config[section][key]
 
-with tempfile.NamedTemporaryFile(mode="w") as tf:
-    tf.write(json.dumps(jsonConfigs))
-    tf.flush()
+with NamedTemporaryFile(mode="w") as temp_file:
+    dump(configs, temp_file)
+    temp_file.flush()
+
     print(
         check_output(
             [
                 "nix-instantiate",
                 "--expr",
                 "--eval",
-                f'builtins.fromJSON (builtins.readFile "{tf.name}")',
+                f'builtins.fromJSON (builtins.readFile "{temp_file.name}")',
             ],
             text=True,
         )
