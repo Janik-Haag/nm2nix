4bea48ac4e8c6078a9f031f43da2044614d00122
haennes <hannes.hofmuth@gmail.com>
2025-11-29 01:04:25 +0100

    add cd argument which automaticall searches relavant paths

--- nm2nix.py
+++ nm2nix.py
@@ -4,10 +4,39 @@ from os.path import isfile, join
 import configparser
 import tempfile
 import json
+import argparse
+from itertools import chain
 
-path = "./"
+PATHS = [
+    "/run/NetworkManager/system-connections",
+    "/etc/NetworkManager/system-connections",
+]
 
-files = [f for f in listdir(path) if isfile(join(path, f))]
+parser = argparse.ArgumentParser(
+    prog="nm2nix",
+    description="Converts .nmconnection files into nix code",
+)
+
+paths_list_human = ", ".join(PATHS)
+parser.add_argument(
+    "-cd", help=f"wether to cd to {paths_list_human}", action="store_true"
+)
+
+args = parser.parse_args()
+
+
+paths = ["./"]
+if args.cd:
+    paths = PATHS
+
+files = list(
+    chain.from_iterable(
+        [
+            ([join(path, f) for f in listdir(path) if isfile(join(path, f))])
+            for path in paths
+        ]
+    )
+)
 nmfiles = [f for f in files if f.endswith(".nmconnection")]
 
 jsonConfigs = {}
@@ -15,7 +44,7 @@ jsonConfigs = {}
 for i in nmfiles:
     config = configparser.ConfigParser(delimiters=("=",), interpolation=None)
     config.read(i)
-    connection_name = i.removesuffix(".nmconnection")
+    connection_name = i.removesuffix(".nmconnection").split("/")[-1]
     jsonConfigs[connection_name] = {}
     for section in config.sections():
         jsonConfigs[connection_name][section] = {}
